83309a4d0d270f169b9c7bdbde0171ce08f36797
Gaffclant <Gaffclant@gmail.com>
2021-06-03 22:49:30 -0500

    Reqorked everything, is now usable

--- main.py
+++ main.py
@@ -1,38 +1,84 @@
+import os
+import random
 import shutil
+import sys
+from time import sleep
+
 import requests
-from sys import argv
 from bs4 import BeautifulSoup
 
 
-if len(argv) < 2:
-    print("Missing required armument: scp-number\nPlease enter a valid scp")
+def rprint(no):
+    no = str(no)
+    print(no.center(shutil.get_terminal_size().columns))
+
+
+rprint("""\
+                 ,#############,
+                 ##           ##
+             m####             ####m
+          m##*'        mmm        '*##m
+        ###'         mm###mm         '###
+      ###        m#############m        ###
+     ##       m####*'  ###  '*####        ##
+    ##      m####      ###      ####m      ##
+   ##      ####      #######      ####      ##
+  m#      ###'        #####        '###      #m
+  ##     ####           #           ####     ##
+  ##     ###    wwwwwwww wwwwwwww    ###     ##
+  ##     #######'   *       *   '######m      '##
+ ##     *#*'######             ######'*#*     ##
+  ##         '#######m     m#######'         ##
+   *#m          '###############'          m#*
+     ##m ,m,        ''*****''        ,m, m##
+      *##'*###m                   m###*'##*
+            '*#######m     m#######*'
+                   '*#######*'
+                   SCP  READER
+\n\n""")
+
+
+def help():
+    print("Type an scp number to see the entry\n")
+    print("logout: Exit SCP-OS\n")
+    print("help: See this page\n")
+    print('r: random scp\n')
 
 
 def set_num(num):
     num = str(num)
     if len(num) == 1:
-        num = f'00{num}'
+        num += str('00')
     elif len(num) == 2:
-        num = f'0{num}'
-    return int(num)
+        num += str('0')
+    return str(num)
 
 
-def get_scp(num):
-    num = set_num(num)
-    url = f'http://www.scpwiki.com/scp-{num}'
-    page = requests.get(url+num)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    title = soup.find(id='page-title').text.strip()
-    page = soup.findAll(id='page-content')
-    for item in page:
-        object_class = item.find('strong', string="Object Class:").next_sibling.strip()
-        scp = item.find('strong', string="Special Containment Procedures:").next_sibling.strip()
-        desc = item.find('strong', string="Description:").next_sibling.strip()
-        
-    scp = {'title':title,
-           'object_class':object_class,
-           'containment':scp,
-           'description':desc}
-    
-    return scp
-        
+help()
+while True:
+    command = input(f'\n\n:')
+    if command == '':
+        continue
+    elif command == 'help':
+        help()
+    elif command == 'logout':
+        rprint("Shutting down...\n")
+        quit()
+    elif command == '001':
+        rprint('UNAUTHORIZED ACCESS.')
+        quit()
+    else:
+        if command == 'r':
+            command = random.randint(0, 6000)
+            command = set_num(command)
+        URL = f"http://www.scpwiki.com/scp-{command}"
+
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # title = soup.find(id='page-title').text.strip()
+        page = soup.find(id='page-content')
+        for child in page.children:
+            schild = str(child)
+            if schild[0:3] == '<p>':
+                rprint(child.text.strip())
+                print()
